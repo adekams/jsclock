72874615d187320ee91e1b8357a2f12fccb35ca2
AdenikeSalau <kifademi@gmail.com>
2020-06-20 00:59:11 +0100

    JS Clock

---
+++ js/script.js
@@ -0,0 +1,52 @@
+var dayNames = ["Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday", "Sunday"]
+function myClock() {
+    //get current date and from it, extract current hour, current minute and current second
+    var date = new Date();
+    var hr = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    var period = '';
+    var day = date.getDay();
+    var month = date.getMonth();
+    var year = date.getFullYear();
+    
+
+    //if hr less than 10, hr = 0 + hr, if not, hr = hr
+    hr = (hr < 10) ? ('0' + hr) :  hr
+    min = (min < 10) ? ('0' + min) :  min
+    sec = (sec < 10) ? ('0' + sec) :  sec
+    period = (hr < 12) ? "A.M" : "P.M"
+
+    day = (day < 10) ? ('0' + day) :  day
+    month = (month < 10) ? ('0' + month) :  month
+
+    //set backgrounds for different time of the day
+    if(hr >= 7 && hr < 18){
+        //document.getElementById("circle").classList.remove("moon")
+        document.getElementById("circle").classList.add("sun")
+        
+    }if(hr >=18 && hr < 20){
+        document.body.classList.add("evening")
+        document.getElementById("circle").classList.add("sun")
+    }else{
+        document.body.classList.add("dark")
+        document.body.classList.remove("evening")
+        document.getElementById("circle").classList.add("moon")
+    }
+
+    document.getElementById("period").innerHTML = period;
+    
+    var currentTime = hr +" : " + min + " : " + sec;
+    var currentDate = dayNames[date.getDay()] +", "+ day + "-" + month + "-" + year 
+    document.getElementById("jsClock").innerHTML = currentTime
+    document.getElementById("jsDay").innerHTML = currentDate
+    
+    
+    //set timer to update clock every 1s
+    var timeout = setTimeout(() => {
+        myClock()
+    }, 1000);
+    
+}
+
+myClock()
